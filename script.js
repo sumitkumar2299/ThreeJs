d3fd9dd1e6fc27ac7e84241ce2d086821b2fcb86
sumitkumar2299 <sumitofficialme@gmail.com>
2025-05-23 20:38:50 +0530

    learning three js

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("i am happy to learn threejs")
+console.log(THREE);
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(2,2,2);
+const material = new THREE.MeshBasicMaterial({color:"red"});
+
+const box = new THREE.Mesh(geometry,material);
+scene.add(box)
+
+const size = {
+    width:700,
+    height:500
+}
+
+const camera = new THREE.PerspectiveCamera(75,size.width/size.height);
+camera.position.z = 4;
+
+scene.add(camera);
+
+// rendering 
+const target = document.querySelector(".wbgl");
+const renderer = new THREE.WebGLRenderer({canvas:target});
+
+renderer.setSize(size.width,size.height)
+renderer.render(scene,camera);
+
